3f212629fcf8c015bfe54ee01caa96d7cde802d8
predrag-miletic-bpds <miletic.predrag1008@gmail.com>
2026-08-12 15:14:28 +0200

    Finalize BPDS React entrypoint

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,5 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { BpdsPrototype } from './bpds-prototype';
 import './style.css';
-
-createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <BpdsPrototype />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+const root=document.getElementById('root');
+if(root)createRoot(root).render(<BrowserRouter><BpdsPrototype/></BrowserRouter>);
