2f0fdfd4ad08677cb45f7e43c971183402097f92
predrag-miletic-bpds <miletic.predrag1008@gmail.com>
2026-08-12 15:18:24 +0200

    Support Practice Mode route alias

--- src/bpds-prototype.tsx
+++ src/bpds-prototype.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import { Routes,Route,Navigate } from 'react-router-dom';
 import { PracticeMode } from './practice-mode';
-export function BpdsPrototype(){return <Routes><Route path="/practice" element={<PracticeMode/>}/><Route path="*" element={<Navigate to="/practice" replace/>}/></Routes>}
+export function BpdsPrototype(){return <Routes><Route path="/practice" element={<PracticeMode/>}/><Route path="/practice-mode" element={<PracticeMode/>}/><Route path="*" element={<Navigate to="/practice" replace/>}/></Routes>}
